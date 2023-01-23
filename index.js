b966b444c59553a2766a54c4b057b8f748d814c3
Nicholas Brereton <nicholasrbrereton@gmail.com>
2023-01-23 04:21:30 -0500

    commit tree 5491b1af630aae86d8cbdcf3ab17b6a58a9b8243

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution here!
+const cats=["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(appendCat){
+    appendCat= cats.push("Ralph")
+}
+function destructivelyPrependCat(prependCat){
+    prependCat=cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat(removeLastCat){
+    removeLastCat=cats.pop()
+}
+function destructivelyRemoveFirstCat(removeFirstCat){
+    removeFirstCat=cats.shift()
+}
+function appendCat(name) {
+    let newCats = cats.slice();
+    newCats.push(name);
+    return newCats;
+}
+function prependCat(name){
+    let newCats = [name,...cats]
+    return newCats
+}
+function removeLastCat() {
+    let newCats = cats.slice();
+    newCats.pop();
+    return newCats;
+}
+function removeFirstCat(){
+    let newCats = cats.slice();
+    newCats.shift()
+    return newCats
+}
